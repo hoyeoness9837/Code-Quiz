83d452efcf4a230adce712bd0ed45e7cd07b7c69
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-26 01:45:47 -0700

    updated with simple structure of quizcard js

--- Assets/scoreboard.js
+++ Assets/scoreboard.js
@@ -1,4 +1,4 @@
-// sort scores higher scores on top to lower towards bottom. 
+// sort scores higher scores on top to lower towards bottom using Math.max(). 
 
 //Add element to display scores on the scoreboard html.
 
