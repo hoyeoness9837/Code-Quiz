5d6b1f72fbf84fc9811e2e45f18affa6da5638de
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-26 20:05:56 -0700

    added comments

--- Assets/timer.js
+++ Assets/timer.js
@@ -3,26 +3,28 @@
 //score still will be stored in LS.
 const timeDisplay = document.getElementById("js-timeLeft");
 
+//Initial timer setting
 let time = 3;
 let minutes = "";
 let seconds = "";
-
+//timer function setting
 const timer = () => {
   let count = setInterval(function () {
     minutes = parseInt(time / 60);
     seconds = time % 60;
     time--;
-
+/* time display setting */
     timeDisplay.innerHTML = `${minutes < 10 ? `0${minutes}` : minutes}:${
-      seconds < 10 ? `0${seconds}` : seconds
+      seconds < 10 ? `0${seconds}` : 
     }`;
-
+/*terminate timer setting */
     if (time < 0) {
       clearInterval(count);
     }
   }, 1000);
 };
 
+//initiating fuctions
 function init() {
   timer();
 }
