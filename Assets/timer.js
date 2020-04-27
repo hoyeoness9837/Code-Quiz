f46468e6890d99b3c9f2222553dcaf3fcb9627e6
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-26 19:14:40 -0700

    updated timer

--- Assets/timer.js
+++ Assets/timer.js
@@ -1,12 +1,23 @@
 // As click start on the intro page, display timer shows up and run in the quizcard.
 // when time is up, stop the quiz, display alert time is up and show score whatever it has added up. also direct to scoreboard page as user click ok.
 //score still will be stored in LS.
+const timeDisplay = document.getElementById("js-timeLeft");
 
+let count = 90;
 
+function timer() {
+  let seconds = count % 60;
+  let tempMinutes = Math.floor(count / 60);
+  let minutes = tempMinutes % 60;
+  timeDisplay.innerText = `${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
 
-
-
-function init(){
-
+function init() {
+  setInterval(() => {
+    count--;
+    timer();
+  }, 1000);
 }
 init();
